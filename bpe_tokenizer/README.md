605282f1dd50c23e6c7d9ad712aadcea3e699dff
Lakshay <lakshay21060@iiitd.ac.in>
2024-02-01 20:23:02 +0530

    modified to fit the project requirements and add api for saving the merge rules to a file

--- bpe_tokenizer/README.md
+++ bpe_tokenizer/README.md
@@ -7,8 +7,21 @@ class BPE_Tokenizer:
 		filename (str): path to the corpus
 		word_freq_pairs (list[list[list[str], int]]): pairs of tokenized word and its frequency in the corpus
 		vocabulary (list[str]): set of tokenized words in the corpus
-		iterations (int): number of iterations for tokenization loop sequence
+		n_merges (int): number of merges for tokenization loop sequence
+		merge_rules(list[type[str, str]]): stores the merge rules in order
 	
-	def __init__(self, filename, iterations):
+	def __init__(self):
         ''' constructor for the BPE_Tokenizer class '''
+	
+	def learn_vocabulary(self, filename, n_merges):
+		''' learns the split rules and frequencies based on the corpus and n_merges '''
+		''' IMPORTANT: does not override previous learn_vocabulary calls '''
+		Attributes:
+			filename (str): path to the corpus
+			n_merges (int): number of merges for tokenization loop sequence
+	
+	def save_merge_rules(self, filename):
+		''' saves the learnt merge_rules to a file in order '''
+		Attributes:
+			filename (str): path at which the merge_rules will be stored
 ```
--- bpe_tokenizer/bpe_tokenizer.py
+++ bpe_tokenizer/bpe_tokenizer.py
@@ -1,11 +1,12 @@
 class BPE_Tokenizer:
-    def __init__(self, filename, iterations):
-        self.filename = filename
+    def __init__(self):
+        self.filename = ""
         self.word_freq_pairs = []
+        # assuming the vocabulary is english
         self.vocabulary = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_'}
-        self.iterations = iterations
-        self.__build()
+        self.merge_rules = []
+        self.n_merges = 0
 
     def __gen_word_freq_pairs(self):
         tmp_map = dict()
@@ -15,10 +16,11 @@ class BPE_Tokenizer:
                     tmp_map[word] += 1
                 else:
                     tmp_map[word] = 1
-        self.word_freq_pairs = [[[*word+'_'], freq] for word, freq in tmp_map.items()]
+        self.word_freq_pairs.extend([[[*word+'_'], freq] for word, freq in tmp_map.items()])
 
     def __iter_vocabulary(self):
         tmp_map = dict()
+        merge_rule_map = dict()
         for word_freq_pair in self.word_freq_pairs:
             word, freq = word_freq_pair
             for i in range(len(word)-1):
@@ -27,6 +29,8 @@ class BPE_Tokenizer:
                     tmp_map[tmp] += freq
                 else:
                     tmp_map[tmp] = freq
+                if tmp not in merge_rule_map:
+                    merge_rule_map[tmp] = (word[i], word[i+1])
         max_comb_val = ""
         max_comb_freq = 0
         for val, freq in tmp_map.items():
@@ -34,6 +38,7 @@ class BPE_Tokenizer:
                 max_comb_val = val
                 max_comb_freq = freq
         self.vocabulary.add(max_comb_val)
+        self.merge_rules.append(merge_rule_map[max_comb_val])
         for i in range(len(self.word_freq_pairs)):
             word, freq = self.word_freq_pairs[i]
             if len(word) == 1: continue 
@@ -49,15 +54,24 @@ class BPE_Tokenizer:
                     j += 1
             self.word_freq_pairs[i][0] = new_word
                 
-    def __build(self):
+    def learn_vocabulary(self, filename, n_merges):
+        self.filename = filename
+        self.n_merges = n_merges
         self.__gen_word_freq_pairs()
-        for _ in range(self.iterations):
+        for _ in range(self.n_merges):
             self.__iter_vocabulary()
 
+    def save_merge_rules(self, filename):
+        with open(filename, "w") as file:
+            for merge_rule in self.merge_rules:
+                file.write(f"{merge_rule[0]},{merge_rule[1]}\n")
+        
     def debug(self):
         print(self.word_freq_pairs[:100])
         print(self.vocabulary)
 
 if __name__ == "__main__":
-    bpe_tokenizer = BPE_Tokenizer("../dataset/corpus.txt", 500)
+    bpe_tokenizer = BPE_Tokenizer()
+    bpe_tokenizer.learn_vocabulary("../dataset/corpus.txt", 500)
+    bpe_tokenizer.save_merge_rules("tests/merge_rules.txt")
     bpe_tokenizer.debug()
